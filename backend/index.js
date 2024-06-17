177c930d5e523786519513ccaea133e393d657e7
Sudhanshu Ranjan <sudhanshusr007@gmail.com>
2024-06-17 20:48:45 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,4 @@
 require("dotenv").config();
-
-const config = require("./config.json");
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
@@ -11,7 +9,7 @@ const Note = require("./models/note.model");
 
 // Mongoose connection with error handling
 mongoose
-  .connect(config.connectionString, {
+  .connect(process.env.connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -29,8 +27,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "",
-    methods: ["POST","PUT","GET","DELETE"]
+    origin: "*",
   })
 );
 
@@ -133,27 +130,25 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.get("/get-user",authenticateToken,async(req,res)=>{
-  const {user} = req.user;
+app.get("/get-user", authenticateToken, async (req, res) => {
+  const { user } = req.user;
   const isUser = await User.findOne({
-    _id:user._id,
+    _id: user._id,
   });
 
-  if(!isUser){
+  if (!isUser) {
     return res.sendStatus(401);
   }
 
   return res.json({
-    user:{
-      fullName:isUser.fullName,
+    user: {
+      fullName: isUser.fullName,
       email: isUser.email,
-      "_id":isUser._id,
+      _id: isUser._id,
       createdOn: isUser.createdOn,
     },
-    message:""
+    message: "",
   });
-
-
 });
 
 app.post("/add-note", authenticateToken, async (req, res) => {
@@ -277,8 +272,8 @@ app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
       userId: user._id,
     });
     return res.json({
-      error:false,
-      message:'Note deleted successfully',
+      error: false,
+      message: "Note deleted successfully",
     });
   } catch (error) {
     return res.status(500).json({
@@ -288,11 +283,10 @@ app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
   }
 });
 
-app.put("/update-note-pinned/:noteId",authenticateToken,async(req,res)=>{
+app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
   const noteId = req.params.noteId;
   const { isPinned } = req.body;
   const { user } = req.user;
-
 
   try {
     const note = await Note.findOne({ _id: noteId, userId: user._id });
@@ -303,7 +297,7 @@ app.put("/update-note-pinned/:noteId",authenticateToken,async(req,res)=>{
         message: "Note not found",
       });
     }
-    
+
     if (isPinned) note.isPinned = isPinned || false;
 
     await note.save();
@@ -329,44 +323,41 @@ app.use((err, req, res, next) => {
     .json({ error: true, message: "An unexpected error occurred" });
 });
 
-app.get('/search-notes/',authenticateToken,async(req,res)=>{
-  const {user} = req.user;
-  const {query} = req.query;
-  if(!query){
-    return res
-      .status(400)
-      .json({
-        error:true,
-        message:'Search query is required'
-      });
+app.get("/search-notes/", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+  const { query } = req.query;
+  if (!query) {
+    return res.status(400).json({
+      error: true,
+      message: "Search query is required",
+    });
   }
 
-  try{
+  try {
     const matchingNotes = await Note.find({
       userId: user._id,
-      $or:[
+      $or: [
         {
-          title:{$regex: new RegExp(query,'i')}
+          title: { $regex: new RegExp(query, "i") },
         },
         {
-          content:{$regex: new RegExp(query,'i')}
-        }
-      ]
+          content: { $regex: new RegExp(query, "i") },
+        },
+      ],
     });
 
     return res.json({
-      error:false,
-      notes:matchingNotes,
-      message:'Notes matching the search query retirved successfully',
+      error: false,
+      notes: matchingNotes,
+      message: "Notes matching the search query retirved successfully",
     });
-  }
-  catch(error){
+  } catch (error) {
     return res.status(500).json({
       error: true,
-      message: "Internal Server Error"
+      message: "Internal Server Error",
     });
   }
-})
+});
 
 app.listen(8000, () => {
   console.log("Server running on port 8000");
